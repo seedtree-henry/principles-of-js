58425d4fed5bf32155bd48a912f2c88ffc9e8927
Henry KIM <seedtree.henry@gmail.com>
2018-09-22 17:31:22 +1000

    fix a word

--- lesson-7/promise-2-fetch-2.js
+++ lesson-7/promise-2-fetch-2.js
@@ -1,5 +1,5 @@
 var API_KEY = "ad79d8ee2949ec15d357357dcbbd891c";
-var place = "Sydney";
+var location = "Sydney";
 
 var fetchPromise = fetch(
   `https://api.openweathermap.org/data/2.5/weather?q=${place}&APPID=${API_KEY}`
