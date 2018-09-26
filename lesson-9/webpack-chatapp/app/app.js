a7a478f1773219e1ecd52d9b28bcf1fb46c6b313
Henry KIM <seedtree.henry@gmail.com>
2018-09-26 16:47:13 +1000

    socket.emit and socket.on

--- lesson-9/webpack-chatapp/app/app.js
+++ lesson-9/webpack-chatapp/app/app.js
@@ -9,7 +9,7 @@ var socketIO = require("socket.io");
 
 // create an app and a server.
 var app = express();
-var server = http.Server(app);
+var server = http.createServer(app);
 var io = socketIO(server);
 var port = 5000;
 
@@ -17,9 +17,15 @@ app.get("/", function(req, res) {
   res.send("<h1>Real Time Chat</h1>");
 });
 
-io.on("connection", function(socket) {
-  console.log("A user connected!");
-});
+io.on("connection", handleConnections);
+
+function handleConnections(socket) {
+  console.log(socket.id);
+
+  socket.on("newEvent", function(data) {
+    console.log(data);
+  });
+}
 
 server.listen(port, function() {
   console.log(`Listening on port ${port}.`);
--- lesson-9/webpack-chatapp/src/index.js
+++ lesson-9/webpack-chatapp/src/index.js
@@ -1,2 +1,9 @@
 var openSocket = require("socket.io-client");
 const socket = openSocket("http://localhost:5000");
+
+setInterval(emitEvent, 2000);
+
+function emitEvent() {
+  console.log("Emit an event.");
+  socket.emit("newEvent", "Are you listening?");
+}
